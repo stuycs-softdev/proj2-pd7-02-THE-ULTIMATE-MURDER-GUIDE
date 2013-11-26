d4af6340af7c230d09f4f9a56df91c889bbac4c8
Sabrina Khan <sabrina.khan739@gmail.com>
2013-11-25 22:20:44 -0500

    initial

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template,session,redirect,request,url_for
+
+app = Flask(__name__)
+app.secret_key = "ULTIMATEMURDERGUIDE"
+
+@app.route('/',methods=['POST','GET'])
+def home():
+if request.method == 'GET':
+        return render_template('Home.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
