9e087453cbaf0886e189a5d67c838f3d3acb31ab
Sabrina Khan <sabrina.khan739@gmail.com>
2013-11-27 01:22:52 -0500

    initial

--- app.py
+++ app.py
@@ -1,24 +1,15 @@
 from flask import Flask
 from flask import render_template,session,redirect,request,url_for
 
-<<<<<<< HEAD
 
-app = Flask(__name__)
-app.secret_key = "BLOGINATOR"
+app=Flask(__name__)
+app.secret_key = "ULTIMATEMURDERHUIDE"
 
-
-@app.route('/',methods=['POST','GET'])
+@app.route('/',methods=["POST","GET"])
 def home():
-=======
-app = Flask(__name__)
-app.secret_key = "ULTIMATEMURDERGUIDE"
-
-@app.route('/',methods=['POST','GET'])
-def home():
-if request.method == 'GET':
->>>>>>> master
-        return render_template('Home.html')
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
+        if request.method == 'GET':
+                return render_template("Home.html")
+                 
+if __name__ == "__main__":
+        app.debug = True
+        app.run()
