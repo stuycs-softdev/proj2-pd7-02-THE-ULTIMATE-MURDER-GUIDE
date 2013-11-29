67355ec94fbf411c6238232a2964ad22b584b5d3
Kelvin <kelvin@ubuntu.(none)>
2013-11-28 20:45:06 -0500

    tried to match weather

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask
 from flask import render_template,session,redirect,request,url_for
-
+import requests
 
 app=Flask(__name__)
 app.secret_key = "ULTIMATEMURDERGUIDE"
 
-@app.route('/',methods=["POST","GET"])
+@app.route('/home',methods=["POST","GET"])
 def home():
         if request.method == 'GET':
                 return render_template("Home.html")
 @app.route('/weather',methods=["POST","GET"])
 def weather():
         if request.method == 'GET':
-                return render_template("Weather.html")
+		blah =  "http://api.wunderground.com/api/01c5e2f1dd1d7086/conditions/q/11209.json"
+                return render_template("Weather.html", blah = blah)
 @app.route('/maps',methods=["POST","GET"])
 def maps():
         if request.method == 'GET':
