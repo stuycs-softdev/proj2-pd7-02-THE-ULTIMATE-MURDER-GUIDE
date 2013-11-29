2f7cb1674669fb10258ea14183004f467ed01af8
Kelvin <kelvin@ubuntu.(none)>
2013-11-29 00:45:41 -0500

    edited

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import render_template,session,redirect,request,url_for
 import requests
+import urllib2
+import method
+
 
 app=Flask(__name__)
 app.secret_key = "ULTIMATEMURDERGUIDE"
@@ -12,8 +15,12 @@ def home():
 @app.route('/weather',methods=["POST","GET"])
 def weather():
         if request.method == 'GET':
-		blah =  "http://api.wunderground.com/api/01c5e2f1dd1d7086/conditions/q/11209.json"
-                return render_template("Weather.html", blah = blah)
+		connection = urllib2.urlopen('http://api.wunderground.com/api/01c5e2f1dd1d7086/conditions/q/11209.json')
+		response = connection.read()
+		a = method.temperature(response)
+		b = method.winds(response)
+		c = method.rainfall(response)
+                return render_template("Weather.html", temp = a, winds = b, rainfall = c)
 @app.route('/maps',methods=["POST","GET"])
 def maps():
         if request.method == 'GET':
@@ -23,6 +30,5 @@ def lawyer():
         if request.method == 'GET':
                 return render_template("Lawyer.html")
                  
-if __name__ == "__main__":
-        app.debug = True
-        app.run()
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port =9000)
